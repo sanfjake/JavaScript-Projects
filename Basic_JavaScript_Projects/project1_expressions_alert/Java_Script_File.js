392eaa9131c2795f1ff5899be13bf8028085865d
sanfjake <jake.w.sanford@gmail.com>
2024-06-05 16:47:32 -0600

    push

---
+++ Basic_JavaScript_Projects/project1_expressions_alert/Java_Script_File.js
@@ -0,0 +1,16 @@
+
+document.write("Jesus Saves") //Displays this text
+var A = "This is a string" //assigns variable A
+document.write(A); //displays variable A
+let B = "I am to create a string variable and display it on a webpage. "
+window.alert (B);
+document.write("I am to write a string with a \" to learn how to add \" when using javascript. ");
+document.write("\"Be who you are and say what you feel,"
++"beacuse those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr.Seuss");
+var C = "Artic Cat" + "Polaris" + "Skidoo"
+document.write(C);
+var Family = "Sanford\'s", Dad = "Jake", Mom = "Erica", Son = "Logan and Kyler", Daughter = "Lacey"
+document.write(Dad);
+document.write(Son);
+
