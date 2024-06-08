bddcca3eff07c84648c4181aa8cf2417ed4c006f
sanfjake <jake.w.sanford@gmail.com>
2024-06-07 18:27:19 -0600

    update

---
+++ Project2_Functions/main.js
@@ -0,0 +1,8 @@
+function myFunction(button) {
+    var sentence = " I am learning";
+    sentence += " from this book I am reading.";
+    document.getElementById("1").innerHTML = sentence;
+}
+function sum(a, b){
+    console.log(a + b)
+}
