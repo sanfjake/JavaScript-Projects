cf4287bbe6c73ed82f76a0622078a563e9ec6d3d
sanfjake <jake.w.sanford@gmail.com>
2024-06-11 09:26:52 -0600

    update

---
+++ Project5_comparisions_type_coercion/main.js
@@ -0,0 +1,32 @@
+document.write("10" + 5);
+
+function my_Function() {
+    document.getElementById("Test1") .innerHTML = 0/0;
+}
+
+function my_Function(){
+document.getElementById("Test2") .innerHTML = isNaN('This is a string');
+}
+
+function my_Function() {
+document.getElementById("Test3") .innerHTML = isNaN('007');
+}
+
+function my_Function(){
+document.getElementById("Test4") .innerHTML = isNaN ('800')
+}
+
+document.write(2.55E400);
+
+document.write(-3.7337E400);
+
+document.write(15>5);
+
+document.write(15>50);
+
+console.log(2 + 2);
+
+document.write(10==10);
+
+document.write(3 == 11);
+
