e838aa3291d04f1de0978ab269a198808b9e904f
sanfjake <jake.w.sanford@gmail.com>
2024-06-13 08:37:38 -0600

    update

--- Project5_comparisions_type_coercion/main.js
+++ Project5_comparisions_type_coercion/main.js
@@ -26,7 +26,28 @@ document.write(15>50);
 
 console.log(2 + 2);
 
-document.write(10==10);
+document.write(10 == 10);
 
 document.write(3 == 11);
 
+x = 10;
+y = 10;
+document.write(x === y);
+
+a = 5;
+b = 20;
+document.write(a === b);
+
+
+
+document.write(5 > 3 && 6 >4);
+
+document.write(5 > 10 && 6 > 13);
+
+document.write(3 > 1 || 6 > 3);
+
+document.write(10 > 20 || 10 > 9);
+
+function not_Function(){
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
