29500ffced61c0392fddda424b3c6ff89277f102
sanfjake <jake.w.sanford@gmail.com>
2024-06-24 19:31:17 -0600

    update

---
+++ Project10_loops_arrays/main.js
@@ -0,0 +1,9 @@
+Function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
