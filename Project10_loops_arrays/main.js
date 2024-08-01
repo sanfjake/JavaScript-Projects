2efd964e0167c13e45670c313f37e99dd82ba383
sanfjake <jake.w.sanford@gmail.com>
2024-08-01 08:34:35 -0600

    update

--- Project10_loops_arrays/main.js
+++ Project10_loops_arrays/main.js
@@ -26,3 +26,33 @@ function array_Function() {
     array_Pic[3] = " purring";
     doccument.getElementById("Array").innerHTML = "In this picture, the array is " + array_Pic[3] + ".";
 }
+
+function constant_function() {
+    const Video_Game = {type: " Action", brand:"Xbox", color:"black"};
+    Video_Game.color = "white";
+    Video_Game.price = "$499";
+    document.getElementById("Constant").innerHTML = "The cost of the" + Video_Game.type + "was" + Video_Game.price;
+
+}
+
+var = 34;
+document.write(x);
+{
+    let x = 33;
+    document.write("<br> + x");
+}
+document.write("<br> + x");
+
+function pie() {
+    return Math.PI;
+}
+
+let car = {
+    make: "Ford" 
+    model: "Bronco"
+    trim: "Raptor"
+    year: "2024"
+    description : function(){
+        return "The car is a " + this.year + this.color + this.make + this.model + this.trim;
+    }
+};
